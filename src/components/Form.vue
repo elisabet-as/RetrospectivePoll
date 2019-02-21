47a09aa387786fde5f4ee8c246099dcfc36479f0
elisabet-as <elisabet240498@gmail.com>
2019-02-21 10:26:09 +0100

    Start project

---
+++ src/components/Form.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <h1>Formulario</h1>
+        <form>
+            <label for=""></label>
+            <input type="text" name="" id="">
+        </form>
+    </section>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+
+            }
+        },
+    }   
+</script>
